8148fa417b44f0636867f81371dc14dafeba7e38
deprasinn <noltvex376@gmail.com>
2026-01-24 02:27:41 +0300

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -1,11 +1,23 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.once("ready", () => {
-  console.log("Bot çalışıyor!");
+client.once('ready', () => {
+  console.log(`Bot giriş yaptı: ${client.user.tag}`);
 });
 
-client.login(process.env.MTQ2NDM5Mjg1MTEwNTI1NTU1Ng.GoyVC9.fAdJIDMJjvBAT3BCw5Pc_fBnPrqdwoHuPnlsKU);
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong! :ping_pong:');
+  }
+});
+
+client.login('MTQ2NDM5Mjg1MTEwNTI1NTU1Ng.GoyVC9.fAdJIDMJjvBAT3BCw5Pc_fBnPrqdwoHuPnlsKU');
